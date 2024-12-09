dc33e291d5427bd309ea6106348bd61f1a92ea77
Peter Smith <homoctrl@gmail.com>
2024-12-09 13:23:23 +0800

    update FileLogger

--- FileLogger.cs
+++ FileLogger.cs
@@ -1,20 +1,52 @@
 ﻿using System;
 using System.IO;
 
-public class FileLogger
+public class FileLogger : IDisposable
 {
-    private string path;
+    private readonly string path;
+    private FileStream fs;
+    private StreamWriter sw;
+    private bool enabled;
+    private bool disposed;
 
-    public FileLogger(string path_)
+    public FileLogger(string path)
     {
-        path = path_;
+        try
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(path));
+            fs = new FileStream(path, FileMode.Append, FileAccess.Write, FileShare.ReadWrite);
+            sw = new StreamWriter(fs, encoding: System.Text.Encoding.UTF8) { AutoFlush = true };
+        }
+        catch (Exception e) when (e is UnauthorizedAccessException || e is IOException)
+        {
+            MsgBox.ShowMessage("Warning", $"Failed to create log file: {e.Message}");
+            enabled = false;
+            return;
+        }
+
+        enabled = true;
+        disposed = false;
+        this.path = path;
     }
 
     public void LogInfo(string message)
     {
-        using (StreamWriter sw = new StreamWriter(path, append: true, encoding: System.Text.Encoding.UTF8))
+        if (!enabled || disposed) return;
+
+        lock (sw)
         {
-            sw.Write($"{DateTime.Now} [INFO] {message}\n");
+            sw.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss.fff} [INFO] {message}");
+        }
+    }
+
+    public void Dispose()
+    {
+        if (enabled && !disposed)
+        {
+            sw.Dispose();
+            fs.Dispose();
+
+            disposed = true;
         }
     }
 }
